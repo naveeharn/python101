ce50d51c98fa639258984c750b882aba6e7e082e
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:27:54 +0700

    first commit

---
+++ py17_tuples.py
@@ -0,0 +1,15 @@
+# tuple
+# collection which is orderd and unchangeable
+# used to group together related data
+
+student = ("asdf",112,"male")
+
+print("count "+ str(student.count("asdf")))
+print("index "+ str(student.index("male")))
+
+for i in student:
+    print(i)
+
+if 112 in student:
+    print("student have 112")
+   
