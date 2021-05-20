1b89dc668c7bbf9ee170fce6dd03ca6570a12a5c
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:26:01 +0700

    first commit py02

---
+++ py02_variable.py
@@ -0,0 +1,21 @@
+
+name = "Boy"
+print("yeah "+name)
+print(type(name))
+
+firstName = "asdf"
+lastName = "qwert"
+fullname = firstName+""+lastName
+print(fullname)
+
+age = 112
+# print("age : " + age +" "+ type(age)) //Syntax Error
+print("age : " + str(age) + " " + str(type(age)))
+
+decimal = 12.34
+print(decimal)
+print("decimal : "+str(decimal)+" " + str(type(decimal)))
+
+isTrue = True
+print(isTrue)
+print("isTrue : " + str(isTrue) + " " + str(type(isTrue)))
