ea3c9f4bbf223cc1045888cd2f34676a9f3b71dc
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:26:08 +0700

    first commit py03

---
+++ py03_multipleAssignment.py
@@ -0,0 +1,14 @@
+# Multiple Assignment
+# allows us to assign mutiple variables at the same time in one line of code
+
+# //Old Syntax
+# name = "qwert"
+# age = 112
+# attractive = True
+
+# //New Syntax
+name, age, attractive = "asdf", 112, True
+
+print(name)
+print(age)
+print(attractive)
