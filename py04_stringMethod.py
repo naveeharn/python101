a605e4bf1d005e7429eaa3e06813081b6a3f761a
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:26:16 +0700

    first commit py04

---
+++ py04_stringMethod.py
@@ -0,0 +1,26 @@
+
+name = "john cena"
+message = "mamacita"
+string = "112"
+
+print(name)
+print(len(name))
+print(name.find("r"))
+
+print(name.capitalize)
+print(name.capitalize())
+
+print(name.upper)
+print(name.upper())
+
+print(name.isdigit())
+print(string.isdigit())
+
+print(name.isalpha())
+print(message.isalpha())
+print(string.isalpha())
+
+print(name.count("n"))
+print(name.replace("n", "k"))
+
+print(name*4)
