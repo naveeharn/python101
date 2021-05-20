d597a31554c7a678b8844024e87fe06bff9e9138
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:26:23 +0700

    first commit py05

---
+++ py05_TypeCase.py
@@ -0,0 +1,49 @@
+# type casting
+# convert the data type of a value to another data type
+
+x = 1  # int
+y = 2.901  # float
+z = "4"  # string
+
+print(x)
+print(y)
+print(z)
+print("")
+
+print(x)
+print(int(y))
+print(int(z))
+print("")
+
+y = int(y)
+z = int(z)
+
+print(x)
+print(int(y))
+print(int(z))
+print(z*10)
+print("")
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+print(x)
+print(y)
+print(z)
+print("")
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x)
+print(y)
+print(z*4)
+print("")
+
+print("x = "+str(x))
+print("y = "+y)
+print("z = "+z)
+
+print(x, y, z)
