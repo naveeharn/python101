5d716aad6c1414fedcf1bc020cd96e69c10b804a
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:26:38 +0700

    first commit py07

---
+++ py07_mathFunction.py
@@ -0,0 +1,13 @@
+import math
+pi = math.pi
+e = math.e
+
+print(pi)
+print(round(pi))
+print(math.floor(pi))
+print(math.ceil(pi))
+print(abs(e-pi))
+print(math.pow(pi, 2))
+print(math.sqrt(pi*pi))
+print(max(2*math.e, pi))
+print(min(pow(e, pi), pow(pi, e), e-pi))
