4548f0ce8bfc282485086381aa20f7040695f64e
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:27:01 +0700

    first commit py11

---
+++ py11_whileLoop.py
@@ -0,0 +1,14 @@
+# while loop
+# a statement that will execute it's block of code.
+# as long as it's condition remain true
+
+# while 1==1:   //Infinite Loop Syntax
+#     print("wasd")
+
+name = ""
+
+while len(name) == 0:
+    name = str(input("Enter your name : "))
+
+print(name)
+
