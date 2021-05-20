e1092712305dbd350cf8616163252303e148bb9b
naveeharn <naveeharn@hotmail.com>
2021-05-20 23:27:43 +0700

    first commit py16

---
+++ py16_2DLists.py
@@ -0,0 +1,11 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "kebab"]
+dessert = ["cake", "muffin"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
+print(food[0])
+print(food[0][0])
