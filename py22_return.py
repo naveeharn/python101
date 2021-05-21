444cf136f1a63eba3bd924f91f75582454ba9972
naveeharn <naveeharn@hotmail.com>
2021-05-21 23:10:01 +0700

    first commit py22

---
+++ py22_return.py
@@ -0,0 +1,8 @@
+# return
+# Functios send Python values/objects back to the caller.
+# These values/objects are known as the function's return value
+
+def calculate(number_1, number_2):
+    return number_1*number_1 - number_2*number_2
+
+print(calculate(4,5))
