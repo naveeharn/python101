3a493329ea8e577e492ad7ee588559ef1a0caef2
naveeharn <naveeharn@hotmail.com>
2021-05-21 23:03:06 +0700

    first commit py21

---
+++ py21_functions.py
@@ -0,0 +1,12 @@
+# function 
+# a block of code witch is executed only when it is called
+
+def func(number):
+    print("qwert "+str(number+109))
+
+func(3)
+
+def name(firstname,lastname):
+    print("Hello Sir " + str(firstname) + " " + str(lastname))
+
+name("john","mamacita")
