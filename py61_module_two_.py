6a697969f2f26a71f1d2bb8c497967d8b5363b8b
naveeharn <naveeharn@hotmail.com>
2021-05-27 01:03:25 +0700

    first commit py61

---
+++ py61_module_two_.py
@@ -0,0 +1,12 @@
+import py61_main_
+
+py61_main_.main()
+
+# print(__name__)
+
+# print(py61_main_.__name__)
+
+# if __name__ == '__main__':
+#     print("running this module directly")
+# else:
+#     print("running other module indirectly")
